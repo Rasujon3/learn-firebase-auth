042ced5ec8eccb7e5eedd0f80b3c943d28e8b400
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-08-30 20:44:14 +0600

    firebase config
    video 3

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDUbU1GCBD5Hrvf4KjxBi6tfdpAgvEkLVA",
+    authDomain: "ema-jhon-sujon.firebaseapp.com",
+    projectId: "ema-jhon-sujon",
+    storageBucket: "ema-jhon-sujon.appspot.com",
+    messagingSenderId: "320709114316",
+    appId: "1:320709114316:web:17e44fb70ce957c6fb0569"
+  };
+
+  export default firebaseConfig;
