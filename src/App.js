0e382f02d3a25e9e31a1f52478784a68bad96517
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-08-30 23:47:24 +0600

    show name,email,photo
    video : 6

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import  firebase from 'firebase/app';
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
+import { useState } from 'react';
 
 
 // firebase.initializeApp(firebaseConfig);
@@ -14,18 +15,45 @@ if (!firebase.apps.length) {
 
 function App() {
 
+  const [user,setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photo: '',
+
+  })
+
   const provider = new firebase.auth.GoogleAuthProvider();
   const handleSignIn=()=>{
     firebase.auth().signInWithPopup(provider)
     .then(res=>{
       const {displayName,photoURL,email} = res.user;
+      const signedInUser = {
+        isSignedIn:true,
+        name:displayName,
+        email:email,
+        photo:photoURL
+      }
+      setUser(signedInUser);
       console.log(displayName,photoURL,email);
+    })
+    .catch(err=>{
+      console.log(err);
+      console.log(err.message);
     })
   }
 
   return (
     <div >
       <button onClick={handleSignIn}>Sign in</button>
+      {
+        user.isSignedIn && 
+        <div>
+          <p>Welcome, {user.name}</p>
+          <p>Your Email : {user.email}</p>
+          <img style={{width:'50%'}} src={user.photo} alt={user.name} />
+        </div>
+      }
     </div>
   );
 }
