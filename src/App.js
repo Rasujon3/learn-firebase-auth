7511b5b836a53d221ac633606529d6fff64470e8
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-08-30 22:50:27 +0600

    firebase setup in app.js
    npm install --save firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import  firebase from 'firebase/app';
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
   return (
     <div >
-      
+      <button>Sign in</button>
     </div>
   );
 }
